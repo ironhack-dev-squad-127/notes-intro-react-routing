fc83dd9a2924f4d93a5507361bbf0d5a22f11c78
Maxence Bouret <maxence.bouret@gmail.com>
2019-05-03 11:46:19 +0100

    Update

--- src/Users.js
+++ src/Users.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import jsonUsers from './users.json'
 
 export default function Users() {
-  return <h2>Users</h2>;
+  return <div className="Users">
+    <h2>Users</h2>
+    <ul>
+
+      {jsonUsers.map(user => (<li key={user.id}>
+        <Link to={"/users/"+user.id}>
+          {user.gender === "male" ? "👨‍💻 " : "👩‍💻 "}
+          {user.name}
+        </Link>
+      </li>))}
+
+    </ul>
+  </div>;
 }
